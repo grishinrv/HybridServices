33fe99681664562bfdc51df913f4257f2cf4300f
RomanGrishin <roman1988grishin@gmail.com>
2022-08-18 17:27:11 +0300

    Simulate IO call

--- demo/BusinessModule/BusinessModuleImplementation.cs
+++ demo/BusinessModule/BusinessModuleImplementation.cs
@@ -1,12 +1,20 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace BusinessModule
 {
     public class BusinessModuleImplementation : IBusinessModule
     {
-        public Task<double> GetResultAsync()
+        private readonly Random _random = new Random();
+        
+        /// <summary>
+        /// Simulate IO call.
+        /// </summary>
+        public async Task<double> GetResultAsync()
         {
-            throw new System.NotImplementedException();
+            int milliseconds = _random.Next(300, 5000);
+            await Task.Delay(milliseconds);
+            return milliseconds / 1000.0;
         }
     }
 }
